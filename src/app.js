38863af318c7ee240cca8ae99778731d99738c6b
jimmygarces47 <jimmygarces4@gmail.com>
2022-02-09 03:39:41 +0000

    ya lo realice completo

--- src/app.js
+++ src/app.js
@@ -6,26 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  let generateExcuse = () => {
-    let who = ["the dog", "my granma", "his turtle", "my bird"];
-    let what = ["eat", "pissed", "crushed", "broked"];
-    let when = [
-      "before the class",
-      "right in time",
-      "when I finished",
-      "during my lunch",
-      "while I was praying"
-    ];
-    let prueba = who.length;
-    let whoIndex = Math.floor(Math.random() * 3);
-    let whatIndex = Math.floor(Math.random() * 3);
-    let whenIndex = Math.floor(Math.random() * 3);
-    console.log(prueba);
-
-    return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
-  };
-  console.log(generateExcuse());
-
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
 };
+
+//write your code here
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let prueba = who.length;
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  console.log(prueba);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+};
+console.log(generateExcuse());
